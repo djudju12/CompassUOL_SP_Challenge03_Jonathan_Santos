83c4afaf79a1d1d3b88d36d6148fbf2ea691cc82
jonathan <jonathan.willian321@gmail.com>
2023-07-31 17:35:32 -0300

    correcao de um bug

--- ms-auth/src/main/java/com/br/compassuol/sp/challenge/msauth/config/OpenApiConfig.java
+++ ms-auth/src/main/java/com/br/compassuol/sp/challenge/msauth/config/OpenApiConfig.java
@@ -1,6 +1,7 @@
 package com.br.compassuol.sp.challenge.msauth.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
